1a2d2ee637b855fdb739a167385f7d05874d30f8
Manoj444v <cgrocker@Channakeshavas-MacBook-Pro.local>
2021-03-22 04:20:07 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebae: {
+    apiKey: "AIzaSyB44gyQSFwchogch-FaWRtmNek08DQdRoo",
+    authDomain: "internship-project-66.firebaseapp.com",
+    projectId: "internship-project-66",
+    storageBucket: "internship-project-66.appspot.com",
+    messagingSenderId: "620754662176",
+    appId: "1:620754662176:web:2a31b85ebcde06f8fd5d45",
+    measurementId: "G-7ZHXT4VN3Z"
+  }
+};
